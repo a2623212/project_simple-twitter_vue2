81e2ba8577d6ae14eb17bd1bf5391c86518d12b1
a2623212 <a2623212@gmail.com>
2023-04-20 11:31:07 +0900

    fix: add new mock data

--- src/views/main.vue
+++ src/views/main.vue
@@ -47,6 +47,7 @@ export default {
           userAvatar:
             "https://images.generated.photos/jWyOJXviE8Ul5twfCO8mzHKx6tC27x34XoaT_jLmISs/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTg0OTc5LmpwZw.jpg",
           isLiked: true,
+          isFollowing: true,
         },
         {
           tweetId: 200,
@@ -61,6 +62,7 @@ export default {
           userAvatar:
             "https://images.generated.photos/m-ZXHrkQsHcyFCarE7GdBGMKcfD-c9Tng10aXkex5Bc/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjkzMDgzLmpwZw.jpg",
           isLiked: false,
+          isFollowing: true,
         },
         {
           tweetId: 300,
@@ -76,6 +78,103 @@ export default {
           userAvatar:
             "https://images.generated.photos/A0C-iICKwNMbO_YobmNi9q8PzaoK0d3v5mIXwcy7cyQ/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjY1MDAxLmpwZw.jpg",
           isLiked: false,
+          isFollowing: true,
+        },
+        {
+          tweetId: 400,
+          UserId: 4,
+          name: "Emily Yu",
+          image: "",
+          account: "EmilyYu01",
+          description:
+            "Emily is a girl's name of Latin origin. Derived from the Roman namesake 'Aemilia'.",
+          RepliesCount: 65,
+          LikesCount: 39,
+          createdAt: "20220126",
+          userAvatar:
+            "https://images.generated.photos/k60lubuQ7Xxi60qIEy8IjcrV9ok_tqSejtyp1mRhFBE/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTgwMDAxLmpwZw.jpg",
+          isLiked: true,
+          isFollowing: false,
+        },
+        {
+          tweetId: 500,
+          UserId: 5,
+          name: "Charles EY",
+          image: "",
+          account: "Charlesey01",
+          description:
+            "The German boy’s name Charles means “free man.” Charles is a royal name that dates back to the reign of Charlemagne, the first Charles the Great. Ever the well-traveled name, it has graced monarchs in England, France, Spain, Hungary, and Portugal since.",
+          RepliesCount: 138,
+          LikesCount: 9,
+          createdAt: "20210327",
+          userAvatar:
+            "https://images.generated.photos/-W9ewfcxUM-vzTiKVeOX2CQeEDSKHu5gaPoQHhccGdk/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODgzNTE4LmpwZw.jpg",
+          isLiked: false,
+          isFollowing: false,
+        },
+        {
+          tweetId: 600,
+          UserId: 6,
+          name: "Eli Ku",
+          image: "",
+          account: "EliKu01",
+          description:
+            "Prepare for your capacity to love unconditionally to reach new heights upon baby's arrival!",
+          RepliesCount: 298,
+          LikesCount: 3000,
+          createdAt: "20230420",
+          userAvatar:
+            "https://images.generated.photos/hDvA4u8e1eBFqZAvv8p6E91p4X_Tuf8RGOgSuAwG8mE/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTg4OTg2LmpwZw.jpg",
+          isLiked: true,
+          isFollowing: true,
+        },
+        {
+          tweetId: 700,
+          UserId: 7,
+          name: "Kai Nakawu",
+          image: "",
+          account: "KaiNakawu20091101",
+          description:
+            "Kai is a gender-neutral name of Welsh origin, with roots worldwide, including in Greece and Scandinavian countries. Kai means 'keeper of the keys and earth,' making it a grounding choice for a parent laying down foundations with baby. In Hawaii, Kai means 'the sea' while in New Zealand, Kai is the Maori word for 'food.'",
+          RepliesCount: 16780,
+          LikesCount: 38765,
+          createdAt: "20230328",
+          userAvatar:
+            "https://images.generated.photos/8x0_R0LPmMTZzLEH1VyYX61jdZNLGYMLtE7lgIpygZ8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODU2NTA3LmpwZw.jpg",
+          isLiked: true,
+          isFollowing: false,
+        },
+        {
+          tweetId: 800,
+          UserId: 8,
+          name: "Landon Lai",
+          image: "",
+          account: "Landonlailai1",
+          description:
+            "Landon is a gender-neutral name with British origins. This name simply means “long hill” and is more commonly used as a surname.",
+          RepliesCount: 2,
+          LikesCount: 4,
+          createdAt: "20221231",
+          userAvatar:
+            "https://images.generated.photos/34hsZWE4kYqkfGCnCT4Zs1bWX17wutDFrrbM2fvMa4U/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NjUzNzIyLmpwZw.jpg",
+          isLiked: false,
+          isFollowing: false,
+        },
+        {
+          tweetId: 900,
+          UserId: 9,
+          name: "Beau Lin",
+          image: "",
+          account: "beaulin01",
+          description:
+            "Beau is a gender-neutral name of French origin. Traditionally used as a nickname, Beau has become more popular as a given name in its own right in the last 50 years.",
+          RepliesCount: 10,
+          LikesCount: 10,
+          createdAt: "20230401",
+          userAvatar:
+            "https://images.generated.photos/V4ck5WmYnym9m4RuBCqbQLVNmSZoU_RWvHEi3v6_fmU/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTA2NTg4LmpwZw.jpg",
+          isLiked: true,
+          isFollowing: true,
         },
       ],
       currentStatus: {
